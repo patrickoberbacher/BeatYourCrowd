75314359183980f7ba5c29b44d3eed4197cbbdb8
patrickoberbacher <patrick.oberbacher@hotmail.com>
2016-03-29 18:02:21 +0200

    New Project
    New Project

---
+++ BeatYourCrowd/Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BeatYourCrowd.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            var clientId = "b26e2a06e5934f9da0b79c72a086ad78";
+            var responseType = "code";
+            var redirect = @"http://localhost:8888/login/callback";
+            var scope = "user-read-private%20user-read-email";
+            var state = "34fFs29kd09";
+            var url = "https://accounts.spotify.com/authorize/?client_id={0}&response_type={1}&redirect_uri={2}&scope={3}&state={4}";
+
+            var parsedUrl = String.Format(url, clientId, responseType, redirect, scope, state);
+
+            return Redirect(parsedUrl);
+        }
+
+        public ActionResult Callback(string code)
+        {
+            ViewBag.Code = code;
+
+            return Redirect("/Home/Index");
+        }
+    }
+}
---
+++ BeatYourCrowd/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BeatYourCrowd.MvcApplication" Language="C#" %>
